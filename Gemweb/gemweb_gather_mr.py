83b7862146ac0dee259bb47da5257503e8a688cb
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2021-07-06 17:52:01 +0200

    added mrjob

--- Gemweb/gemweb_gather_mr.py
+++ Gemweb/gemweb_gather_mr.py
@@ -1,25 +1,35 @@
+import glob
+
 from mrjob.job import MRJob
 import gemweb
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import pickle
+
 
 class Gemweb_gather(MRJob):
     def mapper_init(self):
-        self.hbase_conf = {}
-        self.mongo_conf = {}
-        self.connection = {}
-        self.data_source = {}
+        fn = glob.glob('*.json')
+        config = pickle.load(open(fn[0]), 'rb')
+        self.hbase_conf = config['hbase']
+        self.mongo_conf = config['mongo_db']
+        self.connection = config['connection']
+        self.data_source = config['data_source']
 
     def mapper(self, _, device):
         # call mapreduce with input file as the supplies and performs the following job
         gemweb.gemweb.connection(self.connection['username'], self.connection['password'], timezone="UTC")
 
-        frequencies = [{'name':'data_15m', 'freq':'quart-horari', 'step': relativedelta(minutes=15)},
-                       {'name':'data_1h', 'freq':'horari','step': relativedelta(hours=1)},
-                       {'name':'data_daily', 'freq': 'diari', 'step': relativedelta(days=1)},
-                       {'name':'data_month', 'freq':'mensual', 'step': relativedelta(months=1)}]
+        frequencies = [{'name': 'data_15m', 'freq': 'quart-horari', 'step': relativedelta(minutes=15)},
+                       {'name': 'data_1h', 'freq': 'horari','step': relativedelta(hours=1)},
+                       {'name': 'data_daily', 'freq': 'diari', 'step': relativedelta(days=1)},
+                       {'name': 'data_month', 'freq': 'mensual', 'step': relativedelta(months=1)}]
 
         user = self.connection['user']
+        if 'timeseries' not in self.connection:
+            self.connection['timeseries'] = {}
+        if device not in self.connection['timeseries']:
+            self.connection['timeseries'][device] = {}
 
         for freq in frequencies:
             try:
@@ -27,21 +37,32 @@ class Gemweb_gather(MRJob):
                                                   date_from=datetime(2019, 1, 1),
                                                   date_to=datetime.now(),
                                                   period=freq['freq'])
+            except:
+                data = []
+            if freq['name'] not in self.connection['timeseries'][device]:
+                self.connection['timeseries'][device][freq['name']] = {}
+
+            self.connection['timeseries'][device][freq['name']]['updated'] = datetime.utcnow()
+
+            if len(data > 0):
                 for d in data:
                     d['building'] = device
                     d['measurement_start'] = int(d['datetime'].timestamp())
-                    d['measurement_end'] = int(( d['datetime'] + freq['step']).timestamp() )
+                    d['measurement_end'] = int(( d['datetime'] + freq['step']).timestamp())
+                # save obtained data to hbase
                 hbase = hbase = connection_hbase(hbase_conf)
                 htable = get_HTable(hbase, "{}_{}_{}".format(data_source["hbase_name"], freq['name'], user), {"v": {}, "info": {}})
                 save_to_hbase(htable, data, [("v", ["value"]), ("info", ["measurement_end"])], row_fields=['building', 'measurement_start'])
 
-            except:
-                pass
-            #             connection['timeseries'][tname] += 1
-            #         except:
-            #             pass
-            # mongo = connection_mongo(mongo_conf)
-            # mongo[data_source['info']].replace_one({"_id": connection["_id"]}, connection)
+                self.connection['timeseries'][device][freq['name']]['datetime_to'] = data[-1]['datetime']
+                if 'datetime_from' not in self.connection['timeseries'][device][freq['name']]:
+                    self.connection['timeseries'][device][freq['name']]['datetime_from'] = data[0]['datetime']
+            else:
+                if not self.connection['timeseries'][device][freq['name']]:
+                    self.connection['timeseries'][device][freq['name']] = {"error": "no data"}
+
+            mongo = connection_mongo(self.mongo_conf)
+            mongo[self.data_source['info']].replace_one({"_id": self.connection["_id"]}, self.connection)
 
 
 if __name__ == '__main__':
--- Gemweb/timeseries_gather.py
+++ Gemweb/timeseries_gather.py
@@ -1,18 +1,54 @@
+from tempfile import NamedTemporaryFile
+
 from pyhive import hive
+import argparse
+import pickle
+import json
+import os
+import sys
+sys.path.append(os.getcwd())
+from utils import *
+from Gemweb.gemweb_gather_mr import Gemweb_gather
+# read config file and send it to mapreduce
+with open("./config.json") as config_f:
+    config = json.load(config_f)
 
-# create supplies hdfs file to perform mapreduce
-create_table_hbase = """CREATE EXTERNAL TABLE supplies_aux(id string, value string) 
-                        STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' 
-                        WITH SERDEPROPERTIES (
-                            'hbase.table.name' = 'raw_data:gemweb_supplies_icaen', 
-                            "hbase.columns.mapping" = ":key,info:cups"
-                        )"""
+mongo = connection_mongo(config['mongo_db'])
+data_source = config['datasources']['gemweb']
+for connection in mongo[data_source['info']].find({}):
+    # connection = mongo[data_source['info']].find_one({})
+    job_config = dict()
+    job_config['connection'] = connection.copy()
+    job_config['config'] = dict()
+    job_config['config']['data_source'] = data_source
+    job_config['config']['mongo_connection'] = config['mongo_db']
+    job_config['config']['hbase_connection'] = config['hbase']
 
-save_id_to_file = """INSERT OVERWRITE DIRECTORY '/tmp/supplies_aux/' SELECT id FROM supplies_aux"""
+    f = NamedTemporaryFile(delete=False, suffix='.json')
+    f.write(pickle.dumps(job_config))
+    f.close()
 
-cursor = hive.Connection("master1.internal", 10000, database="gemweb").cursor()
+    # create supplies hdfs file to perform mapreduce
+    create_table_hbase = """CREATE EXTERNAL TABLE supplies_aux(id string, value string) 
+                            STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' 
+                            WITH SERDEPROPERTIES (
+                                'hbase.table.name' = 'raw_data:gemweb_supplies_icaen', 
+                                "hbase.columns.mapping" = ":key,info:cups"
+                            )"""
 
-cursor.execute(create_table_hbase)
-cursor.execute(save_id_to_file)
+    save_id_to_file = """INSERT OVERWRITE DIRECTORY '/tmp/supplies_aux/' SELECT id FROM supplies_aux limit 10"""
 
+    cursor = hive.Connection("master1.internal", 10000, database="gemweb").cursor()
 
+    cursor.execute(create_table_hbase)
+    cursor.execute(save_id_to_file)
+
+    mr_job = Gemweb_gather(args=[
+        '-r', 'hadoop', 'hdfs://{}'.format("/tmp/supplies_aux/"), '--file', f.name,
+        '--file', 'utils.py#utils.py',
+        '--jobconf', 'mapreduce.job.name=gemweb_import',
+        '--jobconf', 'mapreduce.job.maps=10'
+    ])
+
+    with mr_job.make_runner() as runner:
+        runner.run()

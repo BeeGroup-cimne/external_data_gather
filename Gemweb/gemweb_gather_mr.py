2e00424c35cf018d981138fef0e8027bf992b8c8
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2021-07-07 13:39:48 +0200

    add for launching correctly

--- Gemweb/gemweb_gather_mr.py
+++ Gemweb/gemweb_gather_mr.py
@@ -20,18 +20,13 @@ class Gemweb_gather(MRJob):
     def mapper(self, _, device):
         # call mapreduce with input file as the supplies and performs the following job
         gemweb.gemweb.connection(self.connection['username'], self.connection['password'], timezone="UTC")
-
+        update_info = {"$set": {}}
         frequencies = [{'name': 'data_15m', 'freq': 'quart-horari', 'step': relativedelta(minutes=15)},
                        {'name': 'data_1h', 'freq': 'horari', 'step': relativedelta(hours=1)},
                        {'name': 'data_daily', 'freq': 'diari', 'step': relativedelta(days=1)},
                        {'name': 'data_month', 'freq': 'mensual', 'step': relativedelta(months=1)}]
 
         user = self.connection['user']
-        if 'timeseries' not in self.connection:
-            self.connection['timeseries'] = {}
-        if device not in self.connection['timeseries']:
-            self.connection['timeseries'][device] = {}
-
         for freq in frequencies:
             try:
                 data = gemweb.gemweb.gemweb_query(gemweb.ENDPOINTS.GET_METERING, id_=device,
@@ -40,10 +35,7 @@ class Gemweb_gather(MRJob):
                                                   period=freq['freq'])
             except:
                 data = []
-            if freq['name'] not in self.connection['timeseries'][device]:
-                self.connection['timeseries'][device][freq['name']] = {}
-
-            self.connection['timeseries'][device][freq['name']]['updated'] = datetime.utcnow()
+            update_info['$set']["timeseries.{}.{}.updated".format(device, freq['name'])] = datetime.utcnow()
 
             if len(data) > 0:
                 for d in data:
@@ -55,15 +47,17 @@ class Gemweb_gather(MRJob):
                 htable = get_HTable(hbase, "{}_{}_{}".format(self.data_source["hbase_name"], freq['name'], user), {"v": {}, "info": {}})
                 save_to_hbase(htable, data, [("v", ["value"]), ("info", ["measurement_end"])], row_fields=['building', 'measurement_start'])
 
-                self.connection['timeseries'][device][freq['name']]['datetime_to'] = data[-1]['datetime']
+                update_info['$set']["timeseries.{}.{}.datetime_to".format(device, freq['name'])] = data[-1]['datetime']
+
                 if 'datetime_from' not in self.connection['timeseries'][device][freq['name']]:
-                    self.connection['timeseries'][device][freq['name']]['datetime_from'] = data[0]['datetime']
+                    update_info['$set']["timeseries.{}.{}.datetime_from".format(device, freq['name'])] = data[0][
+                        'datetime']
             else:
                 if not self.connection['timeseries'][device][freq['name']]:
-                    self.connection['timeseries'][device][freq['name']] = {"error": "no data"}
+                    update_info['$set']["timeseries.{}.{}.error".format(device, freq['name'])] = "no data"
 
             mongo = connection_mongo(self.mongo_conf)
-            mongo[self.data_source['info']].replace_one({"_id": self.connection["_id"]}, self.connection)
+            mongo[self.data_source['info']].update_one({"_id": self.connection["_id"]}, update_info)
 
 
 if __name__ == '__main__':

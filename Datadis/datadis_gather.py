373b9da27d1dd433f898430d97e4ae6f6c0292e6
csareyan <>
2021-10-13 08:56:56 +0200

    Code updated

--- Datadis/datadis_gather.py
+++ Datadis/datadis_gather.py
@@ -1,9 +1,15 @@
 import json
-import pandas as pd
 import os
+import argparse
 import sys
 sys.path.append(os.getcwd())
 from utils import *
+
+data_type_source = {"contracts": "contracts_datadis",
+                    "supplies": "supplies_datadis",
+                    "max_power": "max_power_datadis",
+                    "hourly_consumption": "hourly_consumption_datadis",
+                    "quarter_hourly_consumption": "quarter_hourly_consumption_datadis"}
 
 
 def get_config():
@@ -18,25 +24,18 @@ def connection_mongo():
     db = cli[config['mongo_db']['db']]
     return db
 
-def get_data(data_type):
-    conn = connection_mongo()           
-    if data_type == "contracts":
-        res = conn["contracts_datadis"].find({},{"_id:0"}, no_cursor_timeout=True)
-    elif data_type == "supplies":
-        res = conn["supplies_datadis"].find({},{"_id:0"}, no_cursor_timeout=True)
-    elif data_type == "max_power":
-        res = conn["max_power_datadis"].find({},{"_id:0"}, no_cursor_timeout=True)
-    elif data_type == "hourly_consumption":
-        res = conn["hourly_consumption_datadis"].find({},{"_id:0"}, no_cursor_timeout=True)
-    else:
-        res = conn["quarter_hourly_consumption_datadis"].find({},{"_id:0"}, no_cursor_timeout=True)        
 
+def get_data(data_type):
+    conn = connection_mongo()
+    res = conn[data_type_source[data_type]].find({},
+                                                 {"_id:0"},
+                                                 no_cursor_timeout=True)
     data = []
     for i in res:
         item = {}
         for k in i.keys():
             item[k] = i[k]
-        data.append(item)    
+        data.append(item)
     return data
 
 
@@ -47,13 +46,29 @@ def load_datadis_hbase(data_type):
     htable = get_HTable(hbase, HTable, {"info": {}})
     documents = get_data(data_type)
     if data_type in ["contracts", "supplies"]:
-        save_to_hbase(htable, documents, [("info", "all")], row_fields=["u_id","cups"])
+        save_to_hbase(htable,
+                      documents,
+                      [("info", "all")],
+                      row_fields=["u_id", "cups"])
     elif data_type == "max_power":
-        save_to_hbase(htable, documents, [("info", "all")], row_fields=["cups", "date"])
+        save_to_hbase(htable,
+                      documents,
+                      [("info", "all")],
+                      row_fields=["cups", "date"])
     else:
-        save_to_hbase(htable, documents, [("info", "all")], row_fields=["cups", "datetime"])
-        
-        
+        save_to_hbase(htable,
+                      documents,
+                      [("info", "all")],
+                      row_fields=["cups", "datetime"])
+
 
 if __name__ == "__main__":
-    load_datadis_hbase('contracts')
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d",
+                    "--data_type",
+                    required=True,
+                    help="type of data to import: one of {}".format(data_type_source.keys()))
+    args = vars(ap.parse_args())
+
+    if args['data_type'] in data_type_source.keys():
+        load_datadis_hbase(args['data_type'])

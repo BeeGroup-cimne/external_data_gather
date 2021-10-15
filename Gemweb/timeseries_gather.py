fe56d3a4470c3955c47d482cdaf894bc8e8fc68d
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2021-10-15 12:54:56 +0200

    maybe works

--- Gemweb/timeseries_gather.py
+++ Gemweb/timeseries_gather.py
@@ -17,6 +17,25 @@ mongo = connection_mongo(config['mongo_db'])
 data_source = config['datasources']['gemweb']
 for connection in mongo[data_source['info']].find({}):
     # connection = mongo[data_source['info']].find_one({})
+    # create supplies hdfs file to perform mapreduce
+    hbase_table = f"raw_data:gemweb_supplies_{connection['user']}"
+    hdfs_file = f"supplies_{connection['user']}"
+
+    create_table_hbase = f"""CREATE EXTERNAL TABLE {hdfs_file}(id string, value string)
+                            STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+                            WITH SERDEPROPERTIES (
+                                'hbase.table.name' = '{hbase_table}',
+                                "hbase.columns.mapping" = ":key,info:cups"
+                            )"""
+
+    save_id_to_file = f"""INSERT OVERWRITE DIRECTORY '/tmp/{hdfs_file}/' SELECT id FROM {hdfs_file}"""
+    remove_hbase_table = f"""DROP TABLE {hdfs_file}"""
+    cursor = hive.Connection("master1.internal", 10000, database="gemweb").cursor()
+    cursor.execute(create_table_hbase)
+    cursor.execute(save_id_to_file)
+    cursor.execute(remove_hbase_table)
+    cursor.close()
+
     job_config = dict()
     job_config['connection'] = connection.copy()
     job_config['config'] = dict()
@@ -28,26 +47,11 @@ for connection in mongo[data_source['info']].find({}):
     f.write(pickle.dumps(job_config))
     f.close()
 
-    # # create supplies hdfs file to perform mapreduce
-    # create_table_hbase = """CREATE EXTERNAL TABLE supplies_aux(id string, value string)
-    #                         STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
-    #                         WITH SERDEPROPERTIES (
-    #                             'hbase.table.name' = 'raw_data:gemweb_supplies_icaen',
-    #                             "hbase.columns.mapping" = ":key,info:cups"
-    #                         )"""
-    #
-    # save_id_to_file = """INSERT OVERWRITE DIRECTORY '/tmp/supplies_aux/' SELECT id FROM supplies_aux limit 1000"""
-    #
-    # cursor = hive.Connection("master1.internal", 10000, database="gemweb").cursor()
-    #
-    # cursor.execute(create_table_hbase)
-    # cursor.execute(save_id_to_file)
-
-    MOUNTS='YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS=/hadoop_stack:/hadoop_stack:ro'
-    IMAGE='YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=beerepo.tech.beegroup-cimne.com:5000/python3-mr'
-    RUNTYPE='YARN_CONTAINER_RUNTIME_TYPE=docker'
+    MOUNTS ='YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS=/hadoop_stack:/hadoop_stack:ro'
+    IMAGE ='YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=beerepo.tech.beegroup-cimne.com:5000/python3-mr'
+    RUNTYPE ='YARN_CONTAINER_RUNTIME_TYPE=docker'
     mr_job = Gemweb_gather(args=[
-        '-r', 'hadoop', 'hdfs://{}'.format("/tmp/supplies_aux/"), '--file', f.name,
+        '-r', 'hadoop', 'hdfs://{}'.format(f"/tmp/{hdfs_file}/"), '--file', f.name,
         '--file', 'utils.py#utils.py',
         '--jobconf', 'mapreduce.map.env={},{},{}'.format(MOUNTS, IMAGE, RUNTYPE),
         '--jobconf', 'mapreduce.reduce.env={},{},{}'.format(MOUNTS, IMAGE, RUNTYPE),

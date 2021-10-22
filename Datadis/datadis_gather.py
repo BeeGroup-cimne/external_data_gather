1e221c67afe98db681ece43088de7c3e3f2708db
csareyan <csareyan@beedataanalytics.com>
2021-10-22 12:13:12 +0200

    Code updated

--- Datadis/datadis_gather.py
+++ Datadis/datadis_gather.py
@@ -28,7 +28,7 @@ def connection_mongo():
 
 def get_cups_list():
     conn = connection_mongo()
-    res = conn["supplies_datadis"].find({})
+    res = conn["supplies_datadis"].find({}, no_cursor_timeout=True)
     return [i["cups"] for i in res]
 
 
@@ -37,7 +37,7 @@ def get_data(data_type, cups = None):
     if not cups: 
         res = conn[data_type_source[data_type]].find({},
                                                      no_cursor_timeout=True)
-    else:
+    else:        
         res = conn[data_type_source[data_type]].find({"cups": cups},
                                                      no_cursor_timeout=True)        
     data = []
@@ -54,45 +54,28 @@ def get_data(data_type, cups = None):
             i["date"] = int(datetime.datetime.strptime(i["date"],"%Y/%m/%d").timestamp())
     elif data_type in ["hourly_consumption","quarter_hourly_consumption"]:
         for i in data:
-            i["datetime"] = i["datetime"].timestamp()                
+            i["datetime"] = int(i["datetime"].timestamp())
     return data
+    
 
-
-def split_list(data_list,list_size):
-    result = []
-    for i in range(0,len(data_list), list_size):
-        result.append(data_list[i:i + list_size])
-    return result
-
-
-def load_datadis_hbase_by_cups(data_type):    
+def load_datadis_hbase_by_cups(data_type):   
+    config = get_config()
     cups_list = get_cups_list()
-    for cups in cups_list:
+    for cups in cups_list:        
         documents = get_data(data_type,cups)
-        if len(documents) > 200000: 
-            documents_partition = self.split_list(documents,200000)        
-            for block in documents_partition:
-                print(cups)
-                try:
-                    config = get_config()
-                    hbase = connection_hbase(config["hbase"])
-                    HTable = 'datadis_' + data_type
-                    htable = get_HTable(hbase, HTable, {"info": {}})
-                    save_to_hbase(htable,
-                                  block,
-                                  [("info", "all")],
-                                  row_fields=["cups", "datetime"])
-                except Exception as e:
-                    print('ERROR load by cups:%s' % e)
-        else:
-            try:
-                save_to_hbase(htable,
-                              documents,
-                              [("info", "all")],
-                              row_fields=["cups", "datetime"])
-            except Exception as e:
-                    print('ERROR load to hbase:%s' % e)    
-
+        print(cups)
+        try:
+            hbase = connection_hbase(config["hbase"])
+            HTable = 'datadis_' + data_type
+            htable = get_HTable(hbase, HTable, {"info": {}})
+            save_to_hbase(htable,
+                          documents,
+                          [("info", "all")],
+                          row_fields=["cups", "datetime"])
+            print('loaded')
+        except Exception as e:
+            print('ERROR load to hbase:%s' % e)    
+            
 
 def load_datadis_hbase(data_type):
     documents = get_data(data_type)
@@ -106,7 +89,8 @@ def load_datadis_hbase(data_type):
                       documents,
                       [("info", "all")],
                       row_fields=["u_id", "cups"])
-    elif data_type == "max_power":
+    else:
+        #max_power
         save_to_hbase(htable,
                       documents,
                       [("info", "all")],
@@ -120,10 +104,12 @@ if __name__ == "__main__":
                     required=True,
                     help="type of data to import: one of {}".format(data_type_source.keys()))
     args = vars(ap.parse_args())
-
     if args['data_type'] in data_type_source.keys():
-        if args['data_type'] in ["hourly_consumption_datadis", 
-                                 "quarter_hourly_consumption_datadis"]:
+        if args['data_type'] in ["hourly_consumption", 
+                                 "quarter_hourly_consumption"]:
             load_datadis_hbase_by_cups(args['data_type'])       
         else:
             load_datadis_hbase(args['data_type'])
+    else:
+        print("Incorrect type")        
+           

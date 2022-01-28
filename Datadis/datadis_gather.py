a772041dd3ec7d3a915689ddd8872aecbae505ae
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2022-01-28 17:21:00 +0100

    corrected some errors with timeseries

--- Datadis/datadis_gather.py
+++ Datadis/datadis_gather.py
@@ -78,7 +78,7 @@ def get_timeseries_data(store, policy, config):
 
     # Get Users to generate the MR input file
     users = get_users(config['neo4j'])
-    local_input = generate_input_tsv(config, users)
+    local_input = generate_input_tsv(config, users[:2])
     input_mr = put_file_to_hdfs(source_file_path=local_input, destination_file_path='/tmp/datadis_tmp/')
     remove_file(local_input)
 
@@ -94,7 +94,7 @@ def get_timeseries_data(store, policy, config):
         '--jobconf', f'mapreduce.map.env={MOUNTS},{IMAGE},{RUNTYPE}',
         '--jobconf', f'mapreduce.reduce.env={MOUNTS},{IMAGE},{RUNTYPE}',
         '--jobconf', f"mapreduce.job.name=datadis_import",
-        '--jobconf', f'mapreduce.job.reduces=2'
+        '--jobconf', f'mapreduce.job.reduces=8'
     ])
     try:
         with datadis_job.make_runner() as runner:
--- Datadis/datadis_gather_mr.py
+++ Datadis/datadis_gather_mr.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import glob
 import pickle
 import bson
@@ -6,12 +6,14 @@ import sys
 from abc import ABC
 
 import pandas as pd
+import pytz
 from mrjob.job import MRJob
 
 from utils import save_to_hbase, mongo_logger, save_to_kafka
 from beedis import ENDPOINTS, datadis
 from dateutil.relativedelta import relativedelta
 
+TZ = pytz.timezone("Europe/Madrid")
 
 class LoginException(Exception):
     pass
@@ -70,16 +72,20 @@ def save_datadis_data(data, credentials, data_type, row_keys, column_map, config
 
 data_types_dict = {
     "data_1h": {
-        "freq_rec": 4,
+        "type_data": "timeseries",
+        "freq_rec": relativedelta(months=2),
         "measurement_type": "0",
         "endpoint": ENDPOINTS.GET_CONSUMPTION,
-        "params": ["cups", "distributor_code", "start_date", "end_date", "measurement_type", "point_type"]
+        "params": ["cups", "distributor_code", "start_date", "end_date", "measurement_type", "point_type"],
+        "elems_in_day": 24,
     },
     "data_15m": {
-        "freq_rec": 1,
+        "type_data": "timeseries",
+        "freq_rec": relativedelta(days=15),
         "measurement_type": "1",
         "endpoint": ENDPOINTS.GET_CONSUMPTION,
-        "params": ["cups", "distributor_code", "start_date", "end_date", "measurement_type", "point_type"]
+        "params": ["cups", "distributor_code", "start_date", "end_date", "measurement_type", "point_type"],
+        "elems_in_day": 96,
     },
     # "max_power": {
     #     "freq_rec": 6,
@@ -112,6 +118,41 @@ def parse_arguments(row, type_params, date_ini, date_end):
     return arguments
 
 
+def download_chunk(data_type, supply, type_params, credentials, config, status):
+    try:
+        date_ini_req = status['date_ini_block'].date()
+        date_end_req = status['date_end_block'].date()
+        sys.stderr.write(f"\t\tObtaining from {date_ini_req} "
+                         f"to {date_end_req}\n")
+        mongo_logger.log(f"\t\tObtaining from {date_ini_req} "
+                         f"to {date_end_req}")
+
+        kwargs = parse_arguments(supply, type_params, date_ini_req, date_end_req)
+        consumption = datadis.datadis_query(type_params['endpoint'], **kwargs)
+        if not consumption:
+            raise GetDataException("No data could be found")
+
+        df_consumption = pd.DataFrame(consumption)
+        df_consumption.index = df_consumption['datetime']
+        df_consumption.sort_index(inplace=True)
+        # Cast datetime64[ns] to timestamp (int64)
+        df_consumption['timestamp'] = df_consumption['datetime'].astype('int64') // 10 ** 9
+
+        # send data to kafka or hbase
+        save_datadis_data(df_consumption.to_dict('records'), credentials, data_type,
+                          ["cups", "timestamp"], [("info", "all")], config, mongo_logger)
+        sys.stderr.write(f"\t\t\tRequest sent\n")
+        mongo_logger.log(f"\t\t\tRequest sent")
+        # store status info
+        status['values'] = df_consumption.shape[0]
+    except GetDataException as e:
+        sys.stderr.write(f"Error gathering data from datadis for user {credentials['username']}: {e}\n")
+        mongo_logger.log(f"Error gathering data from datadis for user {credentials['username']}: {e}")
+    except Exception as e:
+        sys.stderr.write(f"Received and exception: {e}\n")
+        mongo_logger.log(f"Received and exception: {e}")
+
+
 class DatadisMRJob(MRJob, ABC):
     def __read_config__(self):
         fn = glob.glob('*.pickle')
@@ -121,16 +162,18 @@ class DatadisMRJob(MRJob, ABC):
         self.__read_config__()
 
     def mapper(self, _, line):
-        sys.stderr.write(f"Recieved: {line}\n")
         credentials = {k: v for k, v in zip(["username", "password", "user", "namespace"], line.split('\t'))}
         mongo_logger.create(self.config['mongo_db'], self.config['datasources']['datadis']['log'], 'gather',
-                            user=credentials["user"], datasource_user=credentials["username"])
+                            user=credentials["user"], datasource_user=credentials["username"],
+                            log_exec=datetime.utcnow())
         try:
-            mongo_logger.log("Login to datadis")
+            mongo_logger.log(f"Login to datadis")
+            sys.stderr.write(f"Login to datadis\n")
             login(credentials["username"], credentials["password"])
 
             # Obtain supplies from the user logged
-            mongo_logger.log("Obtaining datadis supplies")
+            mongo_logger.log(f"Obtaining datadis supplies")
+            sys.stderr.write(f"Obtaining datadis supplies\n")
             supplies = datadis.datadis_query(ENDPOINTS.GET_SUPPLIES)
             for supply in supplies:
                 supply.update({"nif": credentials['username']})
@@ -139,6 +182,7 @@ class DatadisMRJob(MRJob, ABC):
             log_exported['log_id'] = str(log_exported['log_id'])
             supplies_by_reducer = 4
             id_key = 0
+            mongo_logger.log(f"Obtained: {len(supplies)} supplies")
             sys.stderr.write(f"Obtained: {len(supplies)} supplies\n")
             for i, supply in enumerate(supplies):
                 key = f"{credentials['username']}~{id_key}"
@@ -149,10 +193,10 @@ class DatadisMRJob(MRJob, ABC):
                 yield key, value
 
         except LoginException as e:
-            sys.stderr.write(f"Error in login to datadis for user {credentials['username']}: {e}")
+            sys.stderr.write(f"Error in login to datadis for user {credentials['username']}: {e}\n")
             mongo_logger.log(f"Error in login to datadis for user {credentials['username']}: {e}")
         except Exception as e:
-            sys.stderr.write(f"Received and exception: {e}")
+            sys.stderr.write(f"Received and exception: {e}\n")
             mongo_logger.log(f"Received and exception: {e}")
 
     def reducer_init(self):
@@ -161,14 +205,15 @@ class DatadisMRJob(MRJob, ABC):
     def reducer(self, key, values):
         # Loop supplies
         values = list(values)
-        sys.stderr.write(f"Recieved: {key}, {[x['supply']['cups'] for x in values]}\n")
+        sys.stderr.write(f"Received: {key}, {[x['supply']['cups'] for x in values]}\n")
         for info in values:
             supply = info['supply']
             credentials = info['credentials']
             import_log = info['logger']
-            sys.stderr.write(f"Processing: {supply['cups']}\n")
             import_log['log_id'] = bson.objectid.ObjectId(import_log['log_id'])
             mongo_logger.import_log(import_log, "gather")
+            sys.stderr.write(f"Processing: {supply['cups']}\n")
+            mongo_logger.log(f"Processing: {supply['cups']}")
             datadis_devices = \
                 mongo_logger.get_connection()[self.config['datasources']['datadis']['log_devices']]
             # get the highest page document log
@@ -179,128 +224,77 @@ class DatadisMRJob(MRJob, ABC):
             if not device:
                 # if there is no log document create a new one
                 device = {
-                    "_id": supply['cups'],
-                    "page": 0,
-                    "types": {},
-                    "requests_log": []
+                    "_id": supply['cups']
                 }
-                for t in data_types_dict:
-                    device['types'][t] = {
-                        "date_ini": None,
-                        "date_end": None,
-                        "status": "OK",
-                        "update": "YES"
-                    }
-            elif len(device['requests_log']) >= 100:
-                # if there is one but with more than 100 records, create a new one with higher page
-                device['page'] += 1
-                device['requests_log'] = []
+            # create the data chuncks we will gather the information for timeseries
+            date_ini = datetime.strptime(supply['validDateFrom'], '%Y/%m/%d').date()
+            now = datetime.today().date() + timedelta(days=1)
+            try:
+                date_end = datetime.strptime(supply['validDateTo'], '%Y/%m/%d').date()
+                if date_end <= now:
+                    date_end = date_end
+                    finished = True
+                else:
+                    raise Exception()
+            except Exception as e:
+                date_end = now
+                finished = False
 
-            request_log = {}
+            for t, type_params in [(x, y) for x, y in data_types_dict.items() if y["type_data"] == "timeseries"]:
+                if t not in device:
+                    device[t] = {}
+                loop_date_ini = date_ini
+                while loop_date_ini < date_end:
+                    current_ini = loop_date_ini
+                    if finished:
+                        current_end = min(date_end, current_ini + type_params['freq_rec'])
+                    else:
+                        current_end = current_ini + type_params['freq_rec']
+                    k = "~".join([current_ini.strftime("%Y-%m-%d"), current_end.strftime("%Y-%m-%d")])
+                    date_ini_block = datetime.combine(current_ini, datetime.min.time())
+                    date_end_block = datetime.combine(current_end, datetime.min.time())
+                    time_diff = TZ.localize(date_end_block) - TZ.localize(date_ini_block)
+                    if k not in device[t]:
+                        device[t].update({
+                            k: {
+                                "date_ini_block": date_ini_block,
+                                "date_end_block": date_end_block,
+                                "values": 0,
+                                "total": (time_diff.days + 1) * type_params['elems_in_day'] + time_diff.seconds // 3600,
+                            }
+                        })
+                    loop_date_ini = current_end + relativedelta(days=1)
             try:
                 login(username=credentials['username'], password=credentials['password'])
-                request_log.update({"login": "success"})
             except LoginException as e:
                 sys.stderr.write(f"Error in login to datadis for user {credentials['username']}: {e}\n")
-                request_log.update({"login": "fail"})
                 mongo_logger.log(f"Error in login to datadis for user {credentials['username']}: {e}")
             for data_type, type_params in data_types_dict.items():
-                try:
-                    sys.stderr.write(f"\tType: {data_type}\n")
-                    mongo_logger.log(f"obtaining {data_type} data from datadis")
-                    if type_params['freq_rec'] != "static":
-                        mongo_logger.log(f"the policy is {self.config['policy']}")
-                        if self.config['policy'] == "last" and device['types'][data_type]['update'] == "NO":
-                            sys.stderr.write(f"\t\tIgnoring because it is failed\n")
-                            continue
-                        if self.config['policy'] == "repair" and device['types'][data_type]['status'] == "OK":
-                            sys.stderr.write(f"\t\tIgnoring because it is OK\n")
-                            continue
-                        date_end = datetime.today().date()
-                        sys.stderr.write(f"\t\t{device}\n")
-                        if self.config['policy'] == "last" and device['types'][data_type]['date_end'] is not None:
-                            date_ini = device['types'][data_type]['date_end']
-                        else:
-                            date_ini = datetime.strptime(supply['validDateFrom'], '%Y/%m/%d').date()
-                        sys.stderr.write(f"\t\tObtaining from {date_ini}\n")
-                        # Obtain data
-                        while date_ini < date_end:
-                            current_date = min(date_end, date_ini + relativedelta(months=type_params['freq_rec']))
-                            request_log.update({"date_from": datetime.combine(date_ini, datetime.min.time()),
-                                                "date_to": datetime.combine(current_date, datetime.min.time()),
-                                                "data_type": data_type})
-                            try:
-                                kwargs = parse_arguments(supply, type_params, date_ini, current_date)
-                                sys.stderr.write(f"\t\t\tRequest from {date_ini} to {current_date}\n")
-                                consumption = datadis.datadis_query(type_params['endpoint'], **kwargs)
-                                if not consumption:
-                                    raise Exception("No data could be found")
-                            except Exception as e:
-                                device['types'][data_type]['update'] = "NO"
-                                device['types'][data_type]['status'] = "FAIL"
-                                sys.stderr.write(
-                                    f"Error gathering data from datadis for user {credentials['username']}: {e}\n")
-                                mongo_logger.log(
-                                    f"Error gathering data from datadis for user {credentials['username']}: {e}")
-                                continue
-                            request_log.update({"data_gather": "success"})
-                            df_consumption = pd.DataFrame(consumption)
-                            df_consumption.index = df_consumption['datetime']
-                            df_consumption.sort_index(inplace=True)
-                            # Cast datetime64[ns] to timestamp (int64)
-                            df_consumption['timestamp'] = df_consumption['datetime'].astype('int64') // 10**9
-                            # get first and last time gathered
-                            if device['types'][data_type]['data_ini']:
-                                device['types'][data_type]['data_ini'] = \
-                                    min(device['types'][data_type]['data_ini'],
-                                        df_consumption.iloc[0].datetime.tz_localize(None))
-                            else:
-                                device['types'][data_type]['data_ini'] = \
-                                    df_consumption.iloc[0].datetime.tz_localize(None)
-
-                            if device['types'][data_type]['date_end']:
-                                device['types'][data_type]['date_end'] = \
-                                    max(device['types'][data_type]['date_end'],
-                                        df_consumption.iloc[-1].datetime.tz_localize(None))
-                            else:
-                                device['types'][data_type]['date_end'] = \
-                                    df_consumption.iloc[-1].datetime.tz_localize(None)
-
-                            device['types'][data_type]['status'] = "OK"
-                            save_datadis_data(df_consumption.to_dict('records'), credentials, data_type,
-                                              ["cups", "timestamp"], [("info", "all")], self.config, mongo_logger)
-                            request_log.update({"sent": "success"})
-                            sys.stderr.write(f"\t\t\tRequest sent\n")
-                            self.increment_counter('gathered', 'device', 1)
-                            date_ini = current_date
-                    else:
-                        request_log.update({"data_type": data_type})
+                sys.stderr.write(f"\tType: {data_type}\n")
+                mongo_logger.log(f"\tType {data_type}")
+                if type_params['type_data'] == "timeseries":
+                    if self.config['policy'] == "last":
                         try:
-                            kwargs = parse_arguments(supply, type_params, None, None)
-                            sys.stderr.write(f"\t\tObtaining\n")
-                            data = datadis.datadis_query(type_params['endpoint'], **kwargs)
-                            if not data:
-                                device['types'][data_type]['status'] = "FAIL"
-                                raise Exception("No data could be found")
-                        except Exception as e:
-                            raise GetDataException(f"{e}")
-                        request_log.update({"data_gather": "success"})
-                        for d in data:
-                            d.update({"nif": credentials['username']})
-                        device['types'][data_type]['status'] = "OK"
-                        save_datadis_data(data, credentials, data_type,
-                                          ['cups', 'nif'], [("info", "all")], self.config, mongo_logger)
-                        sys.stderr.write(f"\t\t\tRequest sent\n")
+                            # get last chunk
+                            status = list(device[data_type].values())[-1]
+                        except IndexError as e:
+                            continue
+                        # check if chunk is in current time
+                        if not(status['date_ini_block'] <= datetime.combine(now, datetime.min.time())
+                               <= status['date_end_block']):
+                            continue
+                        download_chunk(data_type, supply, type_params, credentials, self.config, status)
                         self.increment_counter('gathered', 'device', 1)
-                        request_log.update({"sent": "success"})
+                    if self.config['policy'] == "repair":
+                        # get all incomplete chunks
+                        status_list = [x for x in device[data_type].values() if x['values'] < x['total']]
+                        for status in status_list:
+                            download_chunk(data_type, supply, type_params, credentials, self.config, status)
+                            self.increment_counter('gathered', 'device', 1)
 
-                except Exception as e:
-                    sys.stderr.write(f"Received and exception: {e}\n")
-                    mongo_logger.log(f"Received and exception: {e}")
-
-            device['requests_log'].insert(0, request_log)
-            datadis_devices.replace_one({"_id": supply['cups'], "page": device['page']}, device,
-                                        upsert=True)
+            sys.stderr.write(f"finished device\n")
+            mongo_logger.log(f"finished device")
+            datadis_devices.replace_one({"_id": device['_id']}, device, upsert=True)
             self.increment_counter('gathered', 'device', 1)
 
 

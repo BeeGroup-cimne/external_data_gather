beb08928ee405bc877a33d042bb61e4db51a1a89
elskater98 <elskater98@gmail.com>
2022-05-06 17:02:43 +0200

    Add network traffic

--- Ixon/scripts/generate_plots.py
+++ Ixon/scripts/generate_plots.py
@@ -94,115 +94,120 @@ def loss_period_signal_plot(buildings, buildings_type, date_init, date_end):
     fig.savefig(f'./reports/loss_rate/aggregate_buildings.png')
 
 
-# def device_plot(devices, data_init, data_end):
-#     for x in devices:
-#         table = "raw_data:ixon_data_infraestructures"
-#         hbase = happybase.Connection(**config['happybase_con'])
-#         h_table = hbase.table(table)
-#         list1 = []
-#
-#         for k, d in h_table.scan(
-#                 row_start=f"{x['building_id']}~{x['name']}~{int(data_init.timestamp())}".encode("utf-8"),
-#                 row_stop=f"{x['building_id']}~{x['name']}~{int(data_end.timestamp())}".encode("utf-8")):
-#             b, d_, ts = k.decode("utf-8").split("~")
-#             ts = datetime.utcfromtimestamp(float(ts))
-#             try:
-#                 value = float(d[b'v:value'].decode())
-#             except:
-#                 value = np.NAN
-#             list1.append({"ts": ts, "v": value})
-#
-#         if len(list1) <= 0:
-#             print("nodata")
-#             continue
-#
-#         df = pd.DataFrame.from_records(list1)
-#         df.set_index("ts", inplace=True)
-#         df.sort_index()
-#         df = df.resample("15T").mean()
-#         fig = plt.figure(figsize=(15, 10), dpi=80)
-#         fig.suptitle(d_)
-#         plt.plot(df)
-#         # fig.show()
-#         plt.savefig(f'./reports/{devices[0]["building_name"]}/{d_}.png')
-#
-#
-# def ranking_loses(data_init, data_end):
-#     results = list(device_logs.aggregate([{"$match": {"date": {"$gte": data_init,
-#                                                                "$lt": data_end}, "successful": False}},
-#                                           {"$group": {"_id": "$building_name", "count": {"$sum": 1}}},
-#                                           {"$sort": {"count": -1}}]))
-#     if results:
-#         df = pd.DataFrame(results)
-#         print(df)
-#
-#         plt.figure(figsize=(10, 10), dpi=600)
-#         sns.set(font_scale=0.5)
-#
-#         g = sns.barplot(data=df, x="_id", y="count")
-#
-#         plt.xticks(rotation=90)
-#         plt.show()
-#         # plt.savefig('./reports/ranking_24_01_22_30_01_22.png')
-#
-#
-# def network_barplot(data_init, data_end):
-#     for id in building_ids:
-#         l = []
-#         for i in pd.date_range(data_init, data_end, freq="1D"):
-#             try:
-#                 aux_end = i.replace(hour=23, minute=59, second=59)
-#                 list_values = list(network_usage.find({"building": id, "timestamp": {"$gte": i, "$lt": aux_end}},
-#                                                       {"bytes_recv": 1, "bytes_sent": 1, "_id": 0}))
-#                 if list_values:
-#                     df = pd.DataFrame().from_records(list_values)
-#
-#                     x = df.sum()
-#                     l.append({"date": i.date(), "bytes_sent": x['bytes_sent'], 'bytes_recv': x['bytes_recv']})
-#             except:
-#                 pass
-#         if l:
-#             print(id)
-#             df = pd.DataFrame(l)
-#             df.set_index('date', inplace=True)
-#
-#             ax = df.plot(stacked=True, kind='bar')
-#
-#             plt.xlabel('Days')
-#             plt.ylabel('Bytes')
-#             plt.title(id)
-#
-#             plt.xticks(rotation=45)
-#             plt.ticklabel_format(style='plain', axis='y')
-#             plt.tight_layout()
-#             plt.show()
-#
-#
-# def network_usage_plot(date_init, date_end):
-#     for id in building_ids:
-#         list_values = list(network_usage.find({"building": id, "timestamp": {"$gte": date_init, "$lt": date_end}},
-#                                               {"bytes_recv": 1, "bytes_sent": 1, "_id": 0, "timestamp": 1}))
-#         if list_values:
-#             df = pd.DataFrame.from_records(list_values)
-#             # df['total'] = df['bytes_recv'] + df['bytes_sent']
-#             df.set_index('timestamp', inplace=True)
-#             df.plot()
-#
-#             plt.xlabel('Days')
-#             plt.ylabel('Bytes')
-#             plt.title(id)
-#
-#             plt.xticks(rotation=45)
-#             plt.ticklabel_format(style='plain', axis='y')
-#             plt.tight_layout()
-#             # plt.figure(figsize=(10, 10), dpi=600)
-#             plt.show()
-#
+def device_plot(devices, date_init, date_end):
+    for device in devices:
+        table = "raw_data:ixon_data_infraestructures"
+        hbase = happybase.Connection(**config['happybase_con'])
+        h_table = hbase.table(table)
+        list1 = []
+
+        for k, d in h_table.scan(
+                row_start=f"{device['building_id']}~{device['name']}~{int(date_init.timestamp())}".encode("utf-8"),
+                row_stop=f"{device['building_id']}~{device['name']}~{int(date_end.timestamp())}".encode("utf-8")):
+            b, d_, ts = k.decode("utf-8").split("~")
+            ts = datetime.utcfromtimestamp(float(ts))
+            try:
+                value = float(d[b'v:value'].decode())
+            except:
+                value = np.NAN
+            list1.append({"ts": ts, "v": value})
+
+        if len(list1) <= 0:
+            continue
+
+        df = pd.DataFrame.from_records(list1)
+        df.set_index("ts", inplace=True)
+        df.sort_index()
+
+        df = df.resample("15T").mean()
+        df.plot(kind='line')
+
+        plt.xticks(rotation=45)
+        plt.ticklabel_format(style='plain', axis='y')
+        plt.title(d_)
+        plt.tight_layout()
+
+        create_folder(f'reports/devices_ts/{devices[0]["building_name"]}')
+        plt.savefig(f'reports/devices_ts/{devices[0]["building_name"]}/{d_}.png')
+
+
+def ranking_loses(buildings_type, date_init, date_end):
+    results = list(device_logs.aggregate([{"$match": {"date": {"$gte": date_init,
+                                                               "$lt": date_end}, "successful": False}},
+                                          {"$group": {"_id": f"$building_{buildings_type}", "count": {"$sum": 1}}},
+                                          {"$sort": {"count": -1}}]))
+    if results:
+        df = pd.DataFrame(results)
+        df.plot(kind='bar', x="_id", y='count')
+        plt.xticks(rotation=45)
+        plt.ticklabel_format(style='plain', axis='y')
+        plt.tight_layout()
+
+        create_folder(f'reports/loss_ranking/')
+        plt.savefig(f'reports/loss_ranking/{building}_{date_init}_{date_end}.png')
+
+
+def network_daily_traffic(buildings, data_init, data_end):
+    for id in buildings:
+        l = []
+        for i in pd.date_range(data_init, data_end, freq="1D"):
+            try:
+                aux_end = i.replace(hour=23, minute=59, second=59)
+                list_values = list(
+                    network_usage.find({"building": id['building_id'], "timestamp": {"$gte": i, "$lt": aux_end}},
+                                       {"bytes_recv": 1, "bytes_sent": 1, "_id": 0}))
+                if list_values:
+                    df = pd.DataFrame().from_records(list_values)
+
+                    x = df.sum()
+                    l.append({"date": i.date(), "bytes_sent": x['bytes_sent'], 'bytes_recv': x['bytes_recv']})
+            except:
+                pass
+
+        if l:
+            df = pd.DataFrame(l)
+            df.set_index('date', inplace=True)
+
+            ax = df.plot(stacked=True, kind='bar')
+
+            plt.xlabel('Days')
+            plt.ylabel('Bytes')
+            plt.title(id['building_name'])
+
+            plt.xticks(rotation=45)
+            plt.ticklabel_format(style='plain', axis='y')
+            plt.tight_layout()
+
+            create_folder(f'reports/network_aggregate_daily_traffic/')
+            plt.savefig(f'reports/network_aggregate_daily_traffic/{id["building_name"]}.png')
+
+
+def network_usage_plot(buildings, date_init, date_end):
+    for id in buildings:
+        list_values = list(
+            network_usage.find({"building": id['building_id'], "timestamp": {"$gte": date_init, "$lt": date_end}},
+                               {"bytes_recv": 1, "bytes_sent": 1, "_id": 0, "timestamp": 1}))
+        if list_values:
+            df = pd.DataFrame.from_records(list_values)
+            df.set_index('timestamp', inplace=True)
+            df.plot()
+
+            plt.xlabel('Days')
+            plt.ylabel('Bytes')
+            plt.title(id)
+
+            plt.xticks(rotation=45)
+            plt.ticklabel_format(style='plain', axis='y')
+            plt.tight_layout()
+
+            create_folder(f'reports/network_traffic/')
+            plt.savefig(f'reports/network_traffic/{id["building_name"]}_{date_init}_{date_end}.png')
+
 
 if __name__ == '__main__':
     # Arguments
     parser = argparse.ArgumentParser()
-    type_list = ['loss_period', 'network_traffic', 'network_usage']
+    type_list = ['loss_period', 'network_aggregate_daily_traffic', 'network_traffic', 'devices_data', 'loss_ranking']
 
     parser.add_argument("-t", "--type", required=True, type=str, choices=type_list,
                         help="Type of analysis that you want")
@@ -247,22 +252,20 @@ if __name__ == '__main__':
         loss_period_bar_plot(buildings, args.buildings_type, date_init, date_end)
         loss_period_signal_plot(buildings, args.buildings_type, date_init, date_end)
 
-    # if args.type == 'day':
-    #     loss_period_bar_plot(data_init, data_end)
-    #
-    # if args.type == 'signal':
-    #     loss_period_signal_plot(data_init, data_end)
-    #
-    # if args.type == 'devices':
-    #     for building_id in building_ids:
-    #         devices = ixon_devices.find({"building_id": building_id})
-    #         device_plot(list(devices), data_init, data_end)
-    #
-    # if args.type == 'ranking':
-    #     ranking_loses(data_init, data_end)
-    #
-    # if args.type == 'network':
-    #     network_barplot(data_init, data_end)
-    #
-    # if args.type == 'network_usage':
-    #     network_usage_plot(data_init, data_end)
+    if args.type == 'devices_data':
+        for building in buildings:
+            devices = list(ixon_devices.find({f"building_{args.buildings_type}": building}, {'_id': 0}))
+            device_plot(list(devices), date_init, date_end)
+
+    if args.type == 'loss_ranking':
+        ranking_loses(args.buildings_type, date_init, date_end)
+
+    if args.type == 'network_aggregate_daily_traffic':
+        _buildings = [ixon_devices.find_one({f"building_{args.buildings_type}": building},
+                                            {'building_name': 1, 'building_id': 1, '_id': 0}) for building in buildings]
+        network_daily_traffic(_buildings, date_init, date_end)
+
+    if args.type == 'network_traffic':
+        _buildings = [ixon_devices.find_one({f"building_{args.buildings_type}": building},
+                                            {'building_name': 1, 'building_id': 1, '_id': 0}) for building in buildings]
+        network_usage_plot(_buildings, date_init, date_end)

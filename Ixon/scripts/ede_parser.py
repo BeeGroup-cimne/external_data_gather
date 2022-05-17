1b4ea8ec4aa5a298311ccc66c2fc9c62e63e773b
elskater98 <elskater98@gmail.com>
2022-05-17 09:27:52 +0200

    Update ede_parser.py

--- Ixon/scripts/ede_parser.py
+++ Ixon/scripts/ede_parser.py
@@ -1,56 +1,47 @@
+import argparse
 import json
-import sys
 
 import pandas as pd
-from pymongo import MongoClient
+
+from utils import get_json_config, connection_mongo
 
 bacnet_type = {'0': 'analogInput', '1': 'analogOutput', '2': 'analogValue', '3': 'binaryInput', '4': 'binaryOutput',
                '5': 'binaryValue', '13': 'multiStateInput', '14': 'multiStateOutput', '19': 'multiStateValue'}
 
 if __name__ == '__main__':
-    """
-    :arg 1 csv file
-    :arg 2 building_id ej. C0:D3:91:32:E9:B1 - 17089108
-    :arg 3 building_name ej. Primer de Maig
-    """
+    parser = argparse.ArgumentParser()
 
-    if len(sys.argv) < 3:
-        print("python csv_file building_id building_name")
-        exit(-1)
-
-    # Set Config
-    with open('../config.json', 'r') as file:
-        config = json.load(file)
+    parser.add_argument("-f", "--file", required=True, type=str)
+    parser.add_argument("-bi", "--building_id", required=True, type=str)
+    parser.add_argument("-bn", "--building_name", required=True, type=str)
+    parser.add_argument("-ip", "--ip", required=True, type=str)
+    parser.add_argument("-bii", "--building_internal_id", required=True, type=str)
+    args = parser.parse_args()
 
     # MongoDB Connection
-    mongodb_config = config['mongo_db']
-    URI = 'mongodb://%s:%s@%s:%s/%s' % (
-        mongodb_config['user'], mongodb_config['password'], mongodb_config['host'],
-        mongodb_config['port'],
-        mongodb_config['db'])
+    config = get_json_config('../config.json')
+    db = connection_mongo(config['mongo_db'])
 
-    mongo_connection = MongoClient(URI)
-    db = mongo_connection[mongodb_config['db']]
     collection = db['ixon_devices']
 
     # Read CSV
-    input_path = str(sys.argv[1])
-    df = pd.read_csv(input_path, sep=';', encoding='utf-16', skiprows=7)  # Take care encoding
+    input_path = args.file
+    df = pd.read_csv(input_path, sep=';', encoding='utf-8', skiprows=7)  # Take care encoding
 
     # Reduce dataset
-    df = df[['#keyname', 'object-type', 'object-instance', 'description']]
-    df['building_id'] = str(sys.argv[2])
-    df['building_name'] = str(sys.argv[3])
+    df = df[['# keyname', 'object-type', 'object-instance', 'description']]
+    df['building_id'] = args.building_id
+    df['building_name'] = args.building_name
 
     # Rename Column names
     df.rename(
-        columns={'object-instance': 'object_id', 'object-type': 'type', '#keyname': 'name',
+        columns={'object-instance': 'object_id', 'object-type': 'type', '# keyname': 'name',
                  'description': 'description'},
         inplace=True)
 
     df['type'] = df['type'].apply(lambda i: bacnet_type.get(str(i)))
+    df['bacnet_device_ip'] = args.ip
+    df['building_internal_id'] = args.building_internal_id
     df.dropna(subset=['type'], inplace=True)
-
-    # print(df[df['type'].isnull()])
 
     collection.insert_many(json.loads(df.to_json(orient='records')))
